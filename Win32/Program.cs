074d0fadbe48c9477894a30df55c25268428017d
unknown6656 <unknown84@gmx.de>
2022-02-15 09:35:48 +0100

    added main program file

---
+++ Win32/Program.cs
@@ -0,0 +1,17 @@
+using System.Windows.Forms;
+using System;
+
+namespace Win32;
+
+
+public static class Program
+{
+    [STAThread]
+    public static void Main()
+    {
+        Application.SetHighDpiMode(HighDpiMode.SystemAware);
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+        Application.Run(new Form1());
+    }
+}
